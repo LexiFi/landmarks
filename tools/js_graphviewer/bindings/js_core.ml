265e8941780559787b5fb802ce4098cbc4ca2b1c
Marc Lasson <marc.lasson@lexifi.com>
2016-05-09 16:45:06 +0200

    Copyright notices

--- tools/js_graphviewer/bindings/js_core.ml
+++ tools/js_graphviewer/bindings/js_core.ml
@@ -1,3 +1,7 @@
+(* This file is released under the terms of an MIT-like license.     *)
+(* See the attached LICENSE file.                                    *)
+(* Copyright 2016 by LexiFi.                                         *)
+
 module Kinds = struct
   type unknown [@@js]
   module Node = struct
