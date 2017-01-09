5a86fe32544339aa4c01d8b11d3756f155f50c2c
Marc Lasson <marc.lasson@lexifi.com>
2017-01-09 11:17:34 +0100

    Cosmetics

--- testsuite/tests/ppx-bugs/test.ml
+++ testsuite/tests/ppx-bugs/test.ml
@@ -1,7 +1,8 @@
 (* Issue #3 *)
-type c = { x1: float}
+type c = { x1: float }
 let[@landmark] f ~x1 c =
   x1 <= c.x1
 
 (* Issue #5 *)
-let[@landmark "f"] f : type t. t -> t = fun x -> x
+let[@landmark] g : type t. t -> t = fun x -> x
+let[@landmark "h"] h : type t. t -> t = fun x -> x
