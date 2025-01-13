b30b4a4f539009669b6d377de541279bf8ae6b99
edwinans <edyansari5@gmail.com>
2025-01-13 15:24:12 +0100

    Update licenses

--- src/graph.mli
+++ src/graph.mli
@@ -1,6 +1,6 @@
 (* This file is released under the terms of an MIT-like license.     *)
 (* See the attached LICENSE file.                                    *)
-(* Copyright (C) 2000-2024 LexiFi                                    *)
+(* Copyright (C) 2000-2025 LexiFi                                    *)
 
 (** All about exporting profiling results *)
 
