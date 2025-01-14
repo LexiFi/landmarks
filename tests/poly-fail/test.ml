23feb78a9ba1a3a4eda5f74b89f1e56e07f3f2df
edwinans <edyansari5@gmail.com>
2025-01-14 15:19:23 +0100

    Adapt test to ocaml.5.3

--- tests/poly-fail/test.ml
+++ tests/poly-fail/test.ml
@@ -1,7 +1,7 @@
 let ok =
   let test = (fun x -> x)
-  in test "string", test 1;;
+  in test "string", test (Random.int 1);;
 
 let ko =
   let test = (fun x -> x)[@landmark "test"]
-  in test "string", test 1;;
+  in test "string", test (Random.int 1);;
