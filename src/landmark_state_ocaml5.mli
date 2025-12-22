d99696e1f82fe95cf2cdbfdefaf62afda58de221
hra687261 <hra687261@gmail.com>
2025-12-22 13:01:18 +0100

    Remove redundant mli files

--- src/landmark_state_ocaml5.mli
+++
@@ -1,5 +0,0 @@
-(* This file is released under the terms of an MIT-like license.     *)
-(* See the attached LICENSE file.                                    *)
-(* Copyright (C) 2000-2025 LexiFi                                    *)
-
-include Landmark_state_sig.S
