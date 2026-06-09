8c3f33cbc40e167d09d6f2a8d0d89b017b4c1c38
Martin Jambon <martin@mjambon.com>
2026-06-09 02:50:11 +0000

    Undo minor changes

--- tests/js/test.ml
+++ tests/js/test.ml
@@ -11,7 +11,7 @@ let () =
     ~profiling_options:{default_options with format = JSON} ();
   enter main;
   Printf.printf "%d\n%!" (fib 7);
-  Landmark.exit main;
+  exit main;
   if profiling () then begin
     let open Landmark.Graph in
     let cg = export () in
